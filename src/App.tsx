194ace3674189507d01f1d516000b214afe19b64
Marek Kotajny <mkotajny@gmail.com>
2021-12-13 09:59:35 +0100

    feat: add main page
    SN-228

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import './App.css';
 import { useTranslation } from 'react-i18next';
+
+import logo from './assets/images/logo.jpg';
+import styles from './App.module.css';
 
 const App = () => {
   const { t } = useTranslation();
 
   return (
-    <div className='App'>
-      <header className='App-header'>
+    <div className={styles.app}>
+      <header className={styles.appHeader}>
+        <img src={logo} className={styles.appLogo} alt='logo' />
         <p>{t('MAIN.WELCOME')}</p>
         <a
-          className='App-link'
+          className={styles.appLink}
           href='https://github.com/mkotajny/another-react-starter'
           target='_blank'
           rel='noopener noreferrer'
